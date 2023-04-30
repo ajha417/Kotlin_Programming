5e5fd2e79219df824067ff2ad66076f825a53d97
ajha417 <ajha417@rku.ac.in>
2023-04-30 18:18:17 +0530

    updated hello world file

--- src/main/kotlin/hello_world.kt
+++ src/main/kotlin/hello_world.kt
@@ -1,3 +1,6 @@
-    fun main(args: Array<String>){
-
+    fun main(args: Array<String>):Unit{
+//  Unit stands for void
+//        Here the variable name is written first and its data type after it
+//        here print method is used to show output on console
+        println("Hello world")
     }
