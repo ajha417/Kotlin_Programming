18d2b92d0bf8bb22a5e21ada013c816668d91a3f
ajha417 <ajha417@rku.ac.in>
2023-04-30 19:36:08 +0530

    adds string interpolation in function file

--- src/main/kotlin/functions.kt
+++ src/main/kotlin/functions.kt
@@ -1,9 +1,15 @@
 fun main(args:Array<String>) {
     var personName = "Amit Kumar Jha";
     var personObj = Person();
-    personObj.display(personName)
+    personObj.personName = "Amit"
+//    personObj.display(personName)
+    println("The name of the person is ${personObj.personName}");
+    //this is called string interpolation. user $ sign and curly braces
+
+
 }
 class Person{
+    var personName:String = ""
     fun display(name:String){
         print(name)
     }
