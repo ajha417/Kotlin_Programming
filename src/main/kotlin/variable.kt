a33aa1bdfb6e25c272a0fbcc4c3e76853dce9283
ajha417 <ajha417@rku.ac.in>
2023-04-30 18:50:53 +0530

    updates file of variables

--- src/main/kotlin/variable.kt
+++ src/main/kotlin/variable.kt
@@ -1,6 +1,23 @@
 fun main(args:Array<String>){
 
 //    to define variable we need to use var keyword followed by variable name
-    var myVariable = "This is the string values passed"
-    print(myVariable)
+    var myString = "This is the string values passed"
+    println(myString)
+
+//    This is the way to define number
+    var myNumber = 10
+    println(myNumber)
+
+    var myFloat = 10.2
+    println(myFloat)
+
+//    way to declare variable and later using it
+    var myStrin2:String
+    myStrin2 = "Now here we are defining the value of it" //this can be updated later i.e. mutable string
+    println(myStrin2)
+
+//    to define string which cannot be updated use val keyword
+    val myAnotherString = "This is the constant string and can't be reassigned" //Immutable string
+    println(myAnotherString)
+
 }
