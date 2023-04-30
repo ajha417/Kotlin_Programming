efadc9b09e11c849fdd0c0224685950bb8353809
ajha417 <ajha417@rku.ac.in>
2023-04-30 22:57:19 +0530

    adds datatype file

---
+++ src/main/kotlin/dataType.kt
@@ -0,0 +1,34 @@
+fun main(args:Array<String>) {
+    var age:Int = 10 //Int represent the data type is int
+    var age2 = 10 //here it becomes int automatically
+    var name:String = "Amit" //this is string data type
+    var salary:Float = 80000.0F //this is the way to define float value
+    var dobleValue:Double = 48.3 //this is how double value is defined
+
+
+//    boolean data type
+    var isAlive:Boolean = true;
+//    or
+
+    var isStudent = true
+
+ /* kotlin has all its data types as object
+  kotlin has no primitive data type unlike java
+    so the first letter of each data type is capital
+
+
+ data types in kotlin
+ 1. Boolean
+ 2. Char
+ 3. Byte
+ 4. String
+ 5. Int
+ 6. Float
+ 7. Double
+ 8. Long
+
+
+
+
+  */
+}
