0e6a50f5a151cf9cecf05b19bbea9da68cab9b89
ajha417 <ajha417@rku.ac.in>
2023-04-30 18:44:26 +0530

    adds kotlin file containing info about variables

---
+++ src/main/kotlin/variable.kt
@@ -0,0 +1,6 @@
+fun main(args:Array<String>){
+
+//    to define variable we need to use var keyword followed by variable name
+    var myVariable = "This is the string values passed"
+    print(myVariable)
+}
