e0d078b8cbe6bfedae273d9955ce2ee2556c86ff
ajha417 <ajha417@rku.ac.in>
2023-05-01 09:42:47 +0530

    adds iterator file containing info about types of loop and its implementation

---
+++ src/main/kotlin/iterators.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+//    Loops in Kotlin
+
+//    for loop
+    /*for (i in 1..4){
+        println(i)
+    }
+*/
+//    while loop
+    /*var i:Int = 1
+    while(i <= 5){
+        println(i++)
+    }*/
+
+//    do while loop in Kotlin
+    var i:Int = 1
+    do{
+        println(i)
+        i++
+    }while (i <= 2)
+
+//    the basic difference between while loop and do while is that while loop first check the condition and execute
+//    do while no matter what but it will go through 1 iteration and checks the conditions later
+}
--- src/main/kotlin/whenExpression.kt
+++ src/main/kotlin/whenExpression.kt
@@ -7,7 +7,7 @@ fun main(args:Array<String>) {
     when(x){
 //        we can also use range operator
         in 5..20 -> println("x lies in between 5 and 20")
-        
+
         0,1 -> println("x is 0 or 1")
 //        1 -> println("x is 1")
 //        we need not write break keyword
@@ -16,5 +16,19 @@ fun main(args:Array<String>) {
         else -> println("x is unknown")
 
     }
+
+
+//    using when as an expression
+    var value = 10
+    var str:String = when(value){
+        1->"value is 1"
+        2->"value is 2"
+        in 3..10->"value lies in between 3 and 10"
+        else ->{
+            "value is unknown"
+        }
+    }
+    // here when statement returns value so it is acting as expression here
+    println(str)
 
 }
