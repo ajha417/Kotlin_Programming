4a175ce3ca605eff9e7e40fc4c492f1479c88b9f
ajha417 <ajha417@rku.ac.in>
2023-05-04 19:17:02 +0530

    updates default function file

--- src/main/kotlin/defaultFunctions.kt
+++ src/main/kotlin/defaultFunctions.kt
@@ -5,4 +5,9 @@ fun findVolume(length:Int,breadth:Int,height:Int = 10):Int{
 fun main(args: Array<String>) {
     var result = findVolume(2,4);
     println("The volume is $result")
+
+
+//    if we pass all three values then
+    var volume = findVolume(5,10,20);
+    println("The new volume is $volume")
 }
