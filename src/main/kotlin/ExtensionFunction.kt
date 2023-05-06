fbb3d94b3aaeb80fade643a7253e20b265a1ab15
ajha417 <ajha417@rku.ac.in>
2023-05-06 07:06:03 +0530

    update extension function file

--- src/main/kotlin/ExtensionFunction.kt
+++ src/main/kotlin/ExtensionFunction.kt
@@ -1,14 +1,27 @@
 fun main(args: Array<String>) {
 
-    var str1:String = "Hello "
-    var str2:String = "Everyone "
-    var str3:String = "How are you?"
+    val str1:String = "Hello "
+    val str2:String = "Everyone "
+    val str3:String = "How are you?"
 
     println(str1.add(str2 ,str3))
+
+
+//    example to find greater number
+    val x:Int = 5
+    val y:Int = 10
+
+    val greaterNumber = x.greater(y)
+    println("The greater number is: "+greaterNumber)
 }
 
 // Here String is inbuilt class but kotlin provides a way to modify it and add new function to this class
 // so this is example of extension function
 fun String.add(str1:String,str2:String):String{
     return this + str1 + str2
+}
+
+fun Int.greater(other:Int):Int{
+    if(this > other) return this
+    else return other
 }
