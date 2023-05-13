0af46c697fa26746e121d8f1813036309dedc828
ajha417 <ajha417@rku.ac.in>
2023-05-13 17:57:34 +0530

    adds overrding methods

---
+++ src/main/kotlin/Overriding.kt
@@ -0,0 +1,42 @@
+fun main(args: Array<String>) {
+    var car = Cars()
+    car.no_of_wheels = 4
+    car.model = "Mercedez Benz"
+    car.runs()
+    car.speed = 120
+    car.drift()
+    println("The no of wheels in car is ${car.no_of_wheels}")
+    println("The model of car is ${car.model}")
+}
+
+open class Vehicle{
+    var no_of_wheels:Int = -1
+    var model:String = ""
+
+
+    open fun runs(){
+        println("Vehicle runs")
+    }
+}
+
+class Cars:Vehicle(){
+    var speed:Int = -1
+
+//    by default all methods are public final
+//    now to override we need to use open keyword in parent class and override keyword in child class
+    fun drift(){
+        println("car drifts")
+    }
+
+    override fun runs() {
+        super.runs()
+        println("Car runs very fast")
+    }
+}
+
+class Autos:Vehicle(){
+    var mileage:Int = -1
+    fun carriesPassenger(){
+        println("Auto carries passenger")
+    }
+}
