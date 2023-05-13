0ec1f8d21db2777072a6b02eabd05a8089c38498
ajha417 <ajha417@rku.ac.in>
2023-05-13 19:17:31 +0530

    updates inheritance and constructor

--- src/main/kotlin/Inheritance_With_Constructor.kt
+++ src/main/kotlin/Inheritance_With_Constructor.kt
@@ -1,8 +1,9 @@
 fun main(args: Array<String>) {
     var dog = Dogs("Black","Labra")
-    
+
 }
 
+/*
 open class Animals(var color:String){
 
     init {
@@ -17,3 +18,23 @@ class Dogs(color:String,var breed:String):Animals(color){
     }
 }
 
+*/
+
+
+// now implementing with the help of secondary constructor
+
+open class Animals{
+    var color:String = ""
+    constructor(color:String){
+        this.color = color
+        println("From Animals constructor: $color")
+    }
+}
+
+class Dogs:Animals{
+    var breed:String = ""
+    constructor(color:String,breed:String):super(color){
+        this.breed = breed
+        println("From Dogs constructor: $color and $breed")
+    }
+}
