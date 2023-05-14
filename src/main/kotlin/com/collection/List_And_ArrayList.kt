fa3d7ebaa885b7fdfc83c2ee1d091e1e90550943
ajha417 <ajha417@rku.ac.in>
2023-05-14 19:19:57 +0530

    adds mutable and immutable list and arraylist

---
+++ src/main/kotlin/com/collection/List_And_ArrayList.kt
@@ -0,0 +1,27 @@
+package com.collection
+
+fun main(args: Array<String>) {
+
+//    the given list is immutable list, it has fixed size and data can be read only
+    var games = listOf<String>("Cricket","Football","Badminton","Tennis")
+//    games[0] = "ajfdk"  this will give error as it is trying to write an element
+    for(e in games){
+        println(e)
+    }
+
+    println("\n")
+    println("Mutable list")
+    println()
+    var roles = mutableListOf<String>() // it is mutable, no fixed size, can read,write or update data
+    roles.add("Android")
+    roles.add("Web tech")
+    roles.add("Flutter")
+    roles.add("Java")
+    roles.add("SQL")
+    roles.remove("Web tech")
+    roles.set(1,"MERN stack")
+
+    for(index in 0..roles.size-1){
+        println(roles.get(index))
+    }
+}
