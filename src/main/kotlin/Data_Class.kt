321862a7002b61c516e851ef43bef42a4d040e83
ajha417 <ajha417@rku.ac.in>
2023-05-14 10:26:17 +0530

    adds object declaration

--- src/main/kotlin/Data_Class.kt
+++ src/main/kotlin/Data_Class.kt
@@ -2,8 +2,16 @@ fun main(args: Array<String>) {
 
     var u1 = User("Amit",23)
     var u2 = User("Amit",23)
+
+    println(u1.toString())
     if(u1==u2) println("equal")
     else println("not equal")
+
+    var newUser = u1.copy()
+    println(newUser)
+
+    var newUser1 = u1.copy(age=15,name="Rakesh")
+    println(newUser1)
 
 //    data class helps to compare the value only
 }
---
+++ src/main/kotlin/Object_Declaration.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+
+//    no we cannot create an object
+//    we can access data members with the help of dot
+    CustomerData.count = 45
+    println(CustomerData.typeOfCustomer())
+    CustomerData.superClassMethod("Hello boy")
+}
+
+open class MyClass{
+    open fun superClassMethod(str:String){
+        println("This is super class")
+    }
+}
+//kotlin doesn't support static methods and variables
+// but we have alternate for that
+// we can use object keyword instead of class and its data members will behave like static
+
+
+object CustomerData:MyClass(){
+    var count:Int = -1
+    fun typeOfCustomer():String{
+        return "VIP Customer"
+    }
+
+    override fun superClassMethod(str: String){
+        super.superClassMethod(str)
+        println("Object customerData:"+str)
+    }
+}
